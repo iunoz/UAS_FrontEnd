f818a1f3f2fa2258bb8bb46868a252ec24851f02
Tzuryan <ryanhermanto123z@gmail.com>
2024-11-25 22:57:46 +0700

    shop.html berhasil

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,20 +1,22 @@
-console.log('App loaded');
-angular.module('reShoesApp', ['ngRoute'])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: './index.html',
-                controller: 'IndexController'
-            })
-            .when('/about', {
-                templateUrl: './views/about.html',
-                controller: 'AboutController'
-            })
-            .when('/shop', {
-                templateUrl: './views/shop.html',
-                controller: 'ShopController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+// app.js
+// Definisikan modul utama aplikasi
+var app = angular.module('reShoesApp', ['ngRoute']); // 'ngRoute' untuk routing
+
+// Konfigurasi global (opsional)
+app.constant('API_URL', 'https://api.reshoes.com');
+
+angular.module('reShoesApp')
+    .controller('HomeController', function($scope) {
+        // Data yang ingin Anda tampilkan di home.html
+        $scope.message = "Welcome to the ReShoes Home Page!";
+        
+        $scope.product = {
+            name: "ReShoes Air Max 200 GO",
+            price: "$122.15",
+            description: "Comfort and eco-friendly shoes for a better future.",
+            rating: 5
+        };
+        
+        // Tambahkan data lainnya sesuai kebutuhan
     });
+
--- frontend/app/route.js
+++ frontend/app/route.js
@@ -1,18 +1,24 @@
-angular.module('reShoesApp').config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: '/frontend/index.html',
-            controller: 'IndexController'
-        })
-        .when('/about', {
-            templateUrl: './views/about.html',
-            controller: 'AboutController'
-        })  
-        .when('/shop', {
-            templateUrl: './views/shop.html',
-            controller: 'AboutController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-});
+angular.module('reShoesApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/frontend/app/views/home.html',  // Perbarui path sesuai lokasi yang benar
+                controller: 'HomeController'
+            })
+            .when('/about', {
+                templateUrl: '/frontend/app/views/about.html',
+                controller: 'AboutController'
+            })
+            .when('/shop', {
+                templateUrl: '/frontend/app/views/shop.html',
+                controller: 'ShopController'
+            })
+            .when('/home', {
+                templateUrl: '/frontend/app/views/home.html',  // Perbarui path sesuai lokasi yang benar
+                controller: 'HomeController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+
