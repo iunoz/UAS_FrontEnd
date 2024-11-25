110c82c9f07c7a2cf86a8d1b1de708fcb441f931
Tzuryan <ryanhermanto123z@gmail.com>
2024-11-25 21:09:26 +0700

    Index.html

---
+++ frontend/components/footer.component.js
@@ -0,0 +1,6 @@
+angular.module('reShoesApp').component('appFooter', {
+    templateUrl: '/frontend/components/footer.html',
+    controller: function () {
+        // Logika atau data tambahan untuk footer (opsional)
+    }
+}); 
---
+++ frontend/components/navbar.component.js
@@ -0,0 +1,6 @@
+angular.module('reShoesApp').component('navbar', {
+    templateUrl: '/frontend/components/navbar.html',
+    controller: function () {
+        // Logika atau data tambahan untuk navbar (opsional)
+    }
+});
